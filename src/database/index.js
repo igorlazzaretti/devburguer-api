e1cbd3fd731a230de8bdb2a3226e58a3ea1fc370
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-11 15:00:29 -0300

    commit

--- src/database/index.js
+++ src/database/index.js
@@ -2,16 +2,18 @@ import Sequelize from 'sequelize';
 
 import configDatabase from '../config/database';
 
-import User from '../app/models/User';
+import User from '../app/models/user';
+
 const models = {User};
 
 class Database {
+
     constructor(){
         this.init();
     }
     init(){
         this.connection = new Sequelize(configDatabase);
-        models.map((model) => model.init(this.connection));
+        models.map((model) => model.init(this.connection))
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 
 import { Router } from 'express'
 
+import User from './app/models/user';
+
 const routes = new Router()
 
 
-routes.get('/', (request, response) => {
-
-    return response.status(200).json({message: 'Hello'})
-})
+routes.get('/', async (request, response) => {
+    const user = await User.create({
+        name: 'Igor',
+        email: 'idlazzaretti.com',
+        passowrd_hash: 'hauehuehEHUe23'
+    });
+   return response.status(201).json(user); 
+});
 
 module.exports = routes
