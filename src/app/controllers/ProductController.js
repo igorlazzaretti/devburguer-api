28c4dacec8727b123150f5513d4eb4b81065e77f
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-11 22:47:01 -0300

    commit

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,23 @@
+import * as yup from 'yup';
+
+
+
+class ProductController {
+    async store(request, response){
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            price: Yup.number().required(),
+            category: Yup.string().required(),
+        });
+
+        try {
+            await schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+        return response.status(201).json({ message: 'ok'});
+    }
+}
+
+export default new ProductController();
