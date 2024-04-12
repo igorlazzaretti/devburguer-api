2314a144d93b2104b89261bb7ffbe1b4edce5e7c
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-11 22:15:39 -0300

    commit

---
+++ src/database/migrations/20240411234948-create-products-table.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.createTable('products', { 
+        
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true, },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false, },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false },
+      path: {                   // path é o caminho para a Imagem
+        type: Sequelize.STRING },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down (queryInterface ) {
+
+    await queryInterface.dropTable('products');
+
+  }
+};
